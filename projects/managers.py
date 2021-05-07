d4b418cd2feba48a5abc979dc60075445857b782
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2021-05-07 17:25:49 +0200

    managers for project objects

---
+++ projects/managers.py
@@ -0,0 +1,23 @@
+from django import db
+from . import models
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+    
+    
+class ProjectManager(db.models.Manager):
+
+    def create_paginator(self, project_found, per_page, request):
+        """method for paginator generator for product in
+        search.html. We will set with per_page param
+        that only 6 products will be available on 1
+        sheet"""
+
+        paginator = Paginator(project_found, per_page)
+        page_number = request.GET.get("page")
+        try:
+            page_obj = paginator.get_page(page_number)
+        except PageNotAnInteger:
+            page_obj = paginator.get_page(1)
+        except EmptyPage:
+            page_obj = paginator.get_page(paginator.num_pages)
+
+        return page_obj
